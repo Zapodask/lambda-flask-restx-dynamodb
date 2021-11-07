751b330184ef42bdfa5c43acb42c3eeb71fc39bc
Zapodask <joaopedroemerim@gmail.com>
2021-11-07 02:02:01 -0300

    CB

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restx import Api
 
-from .users.routes import api as UserRoutes
+from .users.routes import users as UserRoutes
 
 app = Flask(__name__)
 CORS(app)
